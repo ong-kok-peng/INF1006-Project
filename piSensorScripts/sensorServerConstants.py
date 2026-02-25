dd5cd8df5dfa6cd84d9d0107016511ad1ccb7512
ong-kok-peng <darudestorm77@gmail.com>
2026-02-26 03:32:56 +0800

    i decided not to wayang C program just for the sake of IWSP

---
+++ piSensorScripts/sensorServerConstants.py
@@ -0,0 +1,16 @@
+ADS1115_GAIN = 1
+LIGHT1_RELAY_GPIO = 17		#GPIO 17 to turn on/off relay for light 1
+LIGHT2_RELAY_GPIO = 27		#GPIO 27 to turn on/off relay for light 2
+
+INA199_GAIN = 50
+INA199_SHUNT = 0.05
+
+LDR_VOLTDIVIDER_R1 = 9858
+
+SENSOR_POLL_INTERVAL = 1	#polling interval in seconds
+
+LIGHT1_AMP_LABEL = "Light 1's DC current: "
+LIGHT2_AMP_LABEL = "Light 2's DC current: "
+LDR_LABEL = "LDR Resistance: "
+
+LIGHT_STATUS_LABELS = ["Light 1 on/off: ", "Light 2 on/off: "]
